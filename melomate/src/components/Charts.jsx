4385826a177c790b560f55c7b91248d7a6f8ddcd
laurriana <jehyunie@icloud.com>
2025-01-04 00:05:29 -0500

    improved css and introduced axios fetch requests as demo

---
+++ melomate/src/components/Charts.jsx
@@ -0,0 +1,46 @@
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+
+function Charts() {
+  const [tracks, setTracks] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "http://neocity.local:8084/artists/allPlaycountGreater?playcount=20"
+      );
+      setTracks(response.data);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+const withImage = tracks.filter(song =>
+    song.image && !song.image.includes("lastfm")
+)
+
+  const listItems = withImage.map((artist) => (
+    <div class="card">
+      <img
+        src={artist.image}
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title"> {artist.id}.) {artist.name}</h5>
+        <p class="card-text">
+          he loves me not, he loves me. he holds me tight, then lets me go.
+          <br/> <b>playcount</b>: {artist.playcount}
+        </p>
+        <a href={artist.url} class="btn btn-primary">
+          find on last.fm
+        </a>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="charts">
+        {listItems}
+    </div>);
+}
+
+export default Charts;
