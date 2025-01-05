e76e2b4e7f2c4e15c1d8d8e95410aa6bf450cb5c
laurriana <jehyunie@icloud.com>
2025-01-04 20:40:18 -0500

    fixed incorrect/missing album covers

--- demos/python/patch.py
+++ demos/python/patch.py
@@ -1,17 +1,22 @@
 import requests # type: ignore
+from csv import DictReader
 
-DEFAULT = "http://neocity.local:8084/albums/update/image/{id}"
+PATCH_URL = "http://neocity.local:8084/albums/update/image/{id}"
+GET = "http://neocity.local:8084/albums/{id}"
 
-updates = {
-    "image": "https://ia601208.us.archive.org/33/items/mbid-93ce6f68-159c-497c-bce3-3130ea0faa84/mbid-93ce6f68-159c-497c-bce3-3130ea0faa84-37287548894.jpg"
-}
-
-headers = {
+hrs = {
     "Connection": "close",
     "Content-Type": "application/json"
 }
 
-res = requests.patch(DEFAULT.format(id=113), json=updates, headers=headers)
-print(res.text)
+with open("/Users/lauriana/portfolio/myMeloMateP/demos/no_img.csv", "r") as f:
+    reader = DictReader(f)
+    wrong_imgs = list(reader)
 
-# dictionary with "id": "new url" for faster updates on bulk
+
+for i in wrong_imgs:
+    update = {
+        "image": i["image"]
+    }
+    res = requests.patch(PATCH_URL.format(id=i['id']), json=update, headers=hrs)
+    print(res.text)
