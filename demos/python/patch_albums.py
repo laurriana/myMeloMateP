40e13a98cc4bdefa251de509f0deedda101be687
laurriana <jehyunie@icloud.com>
2025-01-05 03:12:15 -0500

    slight changes before beta testing on backend

---
+++ demos/python/patch_albums.py
@@ -0,0 +1,18 @@
+import requests # type: ignore
+from csv import DictReader
+
+PATCH_URL = "http://neocity.local:8084/tracks/associate/{trackId}/{albumId}"
+
+hrs = {
+    "Connection": "close",
+    "Content-Type": "application/json"
+}
+
+with open("/Users/lauriana/portfolio/myMeloMateP/demos/track_albums.csv", "r") as f:
+    reader = DictReader(f)
+    tracks = list(reader)
+
+
+for i in tracks:
+    res = requests.patch(PATCH_URL.format(trackId=i['track_id'], albumId=i["album_id"]), headers=hrs)
+    print(res.text)
